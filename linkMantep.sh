7bc5c24dd4cd0c5c0be0fb639457e51a82ebe640
TU4NG4L4U <46773642+TU4NG4L4U@users.noreply.github.com>
2019-02-03 17:38:57 +0700

    Add files via upload

---
+++ linkMantep.sh
@@ -0,0 +1,40 @@
+clear
+echo [!]-----------------------------------------[!]
+echo     Recoded By :TUAN G4L4U
+echo     WA         :081514074360
+echo     Github     :https://github.com/TU4NG4L4U
+echo [!]-----------------------------------------[!]
+echo
+echo
+echo=========================================
+echo 1.link Bokep Gratis
+echo=========================================
+echo 2.link Bokep Bayar
+echo=========================================
+echo ""
+echo ""
+echo "\033[34;1m  99\033[37;1m.\033[32;1mKeluar\033[37;1m"
+echo ""
+echo "\033[32;1m ──> \033[32;1m Pilih pilih aja gk usah
+komen!\033[32;1m"
+read -p " TU4N G4L4U---->" pilih
+if [ $pilih = 1 ]
+then
+echo "\033[34;1mLoading.....!!!"
+echo ""
+sleep 1
+echo
+echo
+echo XVideos.com XNXX.com MiaKhalifab.com PusJav.com
+fi
+
+if [ $pilih = 2 ]
+then
+echo "\033[34;1mLoading.....!!!"
+echo ""
+sleep 1
+echo
+echo
+echo BangBos.com Brazzers.com
+echo
+fi
